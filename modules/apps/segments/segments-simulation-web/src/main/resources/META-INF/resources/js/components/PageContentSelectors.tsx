3c3dea7d9f80f6f3c79814c9f34d3751c0af6859
clara izquierdo <claraizquierdosanz@gmail.com>
2023-07-05 11:04:51 +0200

    LPS-187159 Fire and listen to segment change event

--- modules/apps/segments/segments-simulation-web/src/main/resources/META-INF/resources/js/components/PageContentSelectors.tsx
+++ modules/apps/segments/segments-simulation-web/src/main/resources/META-INF/resources/js/components/PageContentSelectors.tsx
@@ -47,6 +47,8 @@ const PREVIEW_OPTIONS = [
 	},
 ];
 
+const SEGMENT_SIMULATION_EVENT = 'SegmenentSimulation:changeSegment';
+
 const MAXIMUM_DROPDOWN_ENTRIES = 8;
 
 function PageContentSelectors({
@@ -87,6 +89,15 @@ function PageContentSelectors({
 
 	const simulateSegmentsEntries = useCallback(() => {
 		if (formRef.current) {
+			Liferay.fire(SEGMENT_SIMULATION_EVENT, {
+				message: sub(
+					Liferay.Language.get('showing-content-for-the-x-x'),
+					[
+						Liferay.Language.get('segment').toLowerCase(),
+						selectedSegmentEntry.name,
+					]
+				),
+			});
 			fetch(simulateSegmentsEntriesURL, {
 				body: new FormData(
 					formRef.current ? formRef.current : undefined
@@ -100,18 +111,31 @@ function PageContentSelectors({
 				}
 			});
 		}
-	}, [simulateSegmentsEntriesURL]);
+	}, [selectedSegmentEntry, simulateSegmentsEntriesURL]);
 
-	const simulateSegmentsExperiment = useCallback((experience) => {
-		const iframe = document.querySelector('iframe');
+	const simulateSegmentsExperiment = useCallback(
+		(experience) => {
+			const iframe = document.querySelector('iframe');
 
-		if (iframe?.contentWindow) {
-			const url = new URL(iframe.contentWindow.location.href);
+			if (iframe?.contentWindow) {
+				Liferay.fire(SEGMENT_SIMULATION_EVENT, {
+					message: sub(
+						Liferay.Language.get('showing-content-for-the-x-x'),
+						[
+							Liferay.Language.get('experience').toLowerCase(),
+							selectedSegmentsExperience.segmentsExperienceName,
+						]
+					),
+				});
 
-			url.searchParams.set('segmentsExperienceId', experience);
-			iframe.src = url.toString();
-		}
-	}, []);
+				const url = new URL(iframe.contentWindow.location.href);
+
+				url.searchParams.set('segmentsExperienceId', experience);
+				iframe.src = url.toString();
+			}
+		},
+		[selectedSegmentsExperience]
+	);
 
 	const handleMoreSegmentEntriesButtonClick = () => {
 		openSelectionModal({
